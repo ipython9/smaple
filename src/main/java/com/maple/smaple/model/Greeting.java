c032648f795c86aa5590f27d12a90b463464fc4c
hanyu6 <hanyu6@asiainfo.com>
2020-03-09 00:03:15 +0800

    spring boot 官方restful 入门指南

---
+++ src/main/java/com/maple/smaple/model/Greeting.java
@@ -0,0 +1,27 @@
+package com.maple.smaple.model;
+
+/**
+ * @Author: hanyu
+ * @Description:
+ * @copyright @hanyu
+ * @Date: Create in  2020/3/8 23:58
+ */
+ public class Greeting {
+
+        private final long id;
+        private final String content;
+
+        public Greeting(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+
