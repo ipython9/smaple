2a166ada2e9fedae2991c96ffecd07b33d12b8c3
hanyu6 <hanyu6@asiainfo.com>
2020-04-14 10:36:33 +0800

    redis lombok

---
+++ src/main/java/com/maple/smaple/model/GoodsEntity.java
@@ -0,0 +1,17 @@
+package com.maple.smaple.model;
+
+import lombok.*;
+
+/**
+ * @Author: hanyu
+ * @Description:
+ * @copyright @hanyu
+ * @Date: Create in  2020/4/13 23:19
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoodsEntity {
+    private String goodId;
+    private String goodName;
+}
--- src/main/java/com/maple/smaple/util/LoggerUtil.java
+++ src/main/java/com/maple/smaple/util/LoggerUtil.java
@@ -1,6 +1,7 @@
 package com.maple.smaple.util;
 
 
+import com.maple.smaple.model.GoodsEntity;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,8 @@ import org.springframework.stereotype.Component;
 public class LoggerUtil {
     @Before("execution(* com.maple.smaple.controller.GreetingController.*(..))")
     public void before(){
+        GoodsEntity goodsEntity = new GoodsEntity();
+        goodsEntity.setGoodId("hello lombok");
         System.out.println("日志");
     }
 }
