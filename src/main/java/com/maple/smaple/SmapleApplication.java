db50e78336d08ffb8737dd5b93f71af6a85d3c03
sakura-maple <435302451@qq.com>
2020-09-02 17:56:12 +0800

    修改

--- src/main/java/com/maple/smaple/SmapleApplication.java
+++ src/main/java/com/maple/smaple/SmapleApplication.java
@@ -11,7 +11,6 @@ public class SmapleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmapleApplication.class, args);
-        String [] s = new String[4];
     }
 
 }
