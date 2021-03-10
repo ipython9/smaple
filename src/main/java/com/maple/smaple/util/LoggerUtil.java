fddf7d606a237508aebed71d0b24f83a27340b32
sakura-maple <435302451@qq.com>
2021-03-11 00:53:03 +0800

    增加junit5 data-es

--- src/main/java/com/maple/smaple/util/LoggerUtil.java
+++ src/main/java/com/maple/smaple/util/LoggerUtil.java
@@ -1,25 +1,25 @@
-package com.maple.smaple.util;
-
-
-import com.maple.smaple.model.GoodsEntity;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
-
-/**
- * @Author: hanyu
- * @Description:设置controller全局日志打印
- * @copyright @hanyu
- * @Date: Create in  2020/4/3 23:34
- */
-
-//@Component
-//@Aspect
-public class LoggerUtil {
-    @Before("execution(* com.maple.smaple.controller.GreetingController.*(..))")
-    public void before(){
-        GoodsEntity goodsEntity = new GoodsEntity();
-        goodsEntity.setGoodId("hello lombok");
-        System.out.println("日志");
-    }
-}
+//package com.maple.smaple.util;
+//
+//
+//import com.maple.smaple.model.GoodsEntity;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Before;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @Author: hanyu
+// * @Description:设置controller全局日志打印
+// * @copyright @hanyu
+// * @Date: Create in  2020/4/3 23:34
+// */
+//
+////@Component
+////@Aspect
+//public class LoggerUtil {
+//    @Before("execution(* com.maple.smaple.controller.GreetingController.*(..))")
+//    public void before(){
+//        GoodsEntity goodsEntity = new GoodsEntity();
+//        goodsEntity.setGoodId("hello lombok");
+//        System.out.println("日志");
+//    }
+//}
--- src/test/java/com/maple/smaple/SmapleApplicationTests.java
+++ src/test/java/com/maple/smaple/SmapleApplicationTests.java
@@ -1,16 +1,60 @@
 package com.maple.smaple;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("测试类")
+//@SpringBootTest
 public class SmapleApplicationTests {
 
+    @DisplayName("测试方法")
     @Test
     public void contextLoads() {
+        System.out.println(1);
     }
 
+    @Test
+    @EnabledOnJre(JRE.JAVA_9)
+    public void assertTest() {
+        String result = "测试";
+        assertEquals("试",result,"测试成功");
+
+    }
+    @DisplayName("参数化测试")
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    void testWithValueSource(int argument) {
+        assertTrue(argument > 0 && argument < 4);
+    }
+
+
+
+
+
+//    @BeforeEach
+//    public void beforeEach(){
+//        System.out.println("每一个测试方法开始前都会执行");
+//
+//    }
+//    @AfterEach
+//    public void afterEach(){
+//        System.out.println("每一个测试方法结束后都会执行");
+//
+//    }
+//    @BeforeAll
+//    public static void beforeAll(){
+//        System.out.println("所有测试方法开始前才会执行");
+//
+//    }
+//    @AfterAll
+//    public static void afterAll(){
+//        System.out.println("所有测试方法结束后才会执行");
+//
+//    }
 }
